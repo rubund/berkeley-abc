74634f3f3f63f1bc662209e48f07c402e353b19d
Ruben Undheim <ruben.undheim@gmail.com>
2014-04-13 01:54:22 +0200

    Added hg revision number to package version

--- createdeb.sh
+++ createdeb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 PACKAGENAME=yosys-abc
-VERSION=0.xx
+VERSION=1.01-hg2058c8ccea68
 rm -rf createdeb/
 mkdir createdeb
 cd createdeb
 mkdir $PACKAGENAME-$VERSION
 cp -r ../abc  $PACKAGENAME-$VERSION/
 cp -r ../README  $PACKAGENAME-$VERSION/
-tar -czf yosys-abc_0.xx.orig.tar.gz $PACKAGENAME-$VERSION
+tar -czf $PACKAGENAME_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
 cp -r -v ../debian $PACKAGENAME-$VERSION/
 cd $PACKAGENAME-$VERSION
 dpkg-buildpackage
