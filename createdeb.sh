7545de4d73d74db8d281ca63cc8d2b669b4b4625
Ruben Undheim <ruben.undheim@gmail.com>
2014-09-05 22:00:03 +0200

    Updated build routine for abc

--- createdeb.sh
+++ createdeb.sh
@@ -7,10 +7,9 @@ mkdir createdeb
 cd createdeb
 rm -rf $PACKAGENAME-$VERSION
 mkdir $PACKAGENAME-$VERSION
-cp -r ../abc  $PACKAGENAME-$VERSION/
-cp -r ../README  $PACKAGENAME-$VERSION/
+cp -r ../abc/*  $PACKAGENAME-$VERSION/
 tar -czf ${PACKAGENAME}_${VERSION}.orig.tar.gz $PACKAGENAME-$VERSION
 #tar -xzf ${PACKAGENAME}_${VERSION}.orig.tar.gz 
 cp -r -v ../debian $PACKAGENAME-$VERSION/
 cd $PACKAGENAME-$VERSION
-dpkg-buildpackage -S
+dpkg-buildpackage
